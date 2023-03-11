715ed9960456555c1b1db42a680a4655e44b45af
Tarek <tarek22042002triki@gmail.com>
2023-03-12 00:27:29 +0100

    token

--- back-end/packages/api/src/routes/student.ts
+++ back-end/packages/api/src/routes/student.ts
@@ -3,6 +3,8 @@ const Router = new HyperExpress.Router();
 import Pouchdb from 'pouchdb';
 import PouchDB_Find from 'pouchdb-find';
 Pouchdb.plugin(PouchDB_Find);
+import jwt from "jsonwebtoken";
+
 
 
 
@@ -10,7 +12,7 @@ Router.post('/login',async(req,res)=>{
 
     const db = new Pouchdb('http://admin:admin@127.0.0.1:5984/labo');
     req.body=await req.json();
-    let id=await db.find({
+    let student=await db.find({
         selector: {
             matricule: {
                 $eq :req.body.matricule
@@ -18,16 +20,12 @@ Router.post('/login',async(req,res)=>{
         }
       });
     
-      if(id.docs == ''){
+      if(student.docs == ''){
         res.status(400).json({ message: 'User does not exist.' })
       }else{
-        res.status(400).json({ message: 'User already exists.' });
+        const token = jwt.sign({ userId: student._id}, "12345");
+      res.status(200).json({ auth: true, token: token, uid: student._id, type: "user" });
       }
-      
-    
-    
-    
-
 })
 
 module.exports=Router;
